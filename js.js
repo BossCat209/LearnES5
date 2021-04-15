8aa9432893dc36a3d1e8a07d1776c67518eded0a
HuanNH <huanhoang2092000@gmail.com>
2021-04-15 10:51:18 +0700

    Array.foeEach()

--- js.js
+++ js.js
@@ -1,11 +1,24 @@
 //string.trim()
 function myClick1() {
-    const str = "      No Oke        ";
+    let str = "      No Oke        ";
     alert(str.trim());
 }
-//Array.isArray
+
+//Array.isArray()
 function myClick2() {
-    const array = [1,2,3,4,5];
-    const a = document.getElementById("Test");
-    a.innerHTML= Array.isArray(array);
+    let array = [1, 2, 3, 4, 5];
+    let a = document.getElementById("Test");
+    a.innerHTML = Array.isArray(array);
+}
+
+//Array.forEach()
+function myClick3() {
+    let array = [6, 9, 69, 96, 96.69];
+    let text = "";
+    array.forEach(myText);
+    document.getElementById("Test1").innerHTML = text;
+    function myText(value) {
+        text = text + value + " ";
+        console.log(text);
+    }
 }
