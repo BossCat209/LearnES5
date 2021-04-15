0b7e6239d083c83d6164e12b20a7a850db24f377
HuanNH <huanhoang2092000@gmail.com>
2021-04-15 09:48:53 +0700

    Array.isArray

--- js.js
+++ js.js
@@ -1,5 +1,11 @@
-//string.him()
-function myClick() {
-    var str = "      No Oke        ";
+//string.trim()
+function myClick1() {
+    const str = "      No Oke        ";
     alert(str.trim());
+}
+//Array.isArray
+function myClick2() {
+    const array = [1,2,3,4,5];
+    const a = document.getElementById("Test");
+    a.innerHTML= Array.isArray(array);
 }
